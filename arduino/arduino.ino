d52779c8586334579a0e9ce80aebe402830b3ee2
Tuan Nguyen <tufourn@gmail.com>
2024-10-03 16:00:46 -0400

    feat: initial arduino skeleton code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,3 +1,41 @@
+enum CommandMode {
+  INSTRUCTION,
+  MASK1,
+  MASK2,
+};
+
+CommandMode cmdMode = INSTRUCTION;
+
+// move stepper x 1 step (dot width) to the right
+// keep track of steps moved so we can return to the begin of line
+void step() {
+  // TODO
+}
+
+// goto the begin of current line
+void gotoBeginLine() {
+  // TODO
+}
+
+// move stepper y 1 line above (dot height) * nozzleCount
+void gotoNextLine() {
+  // TODO
+}
+
+// dispense ink based on bitmask
+// when last mask done, call step() and change cmdMode to INSTRUCTION
+void dispense(unsigned char mask) {
+  if (cmdMode == MASK1) {
+    /* TODO: dispense first set of nozzles */
+    cmdMode = MASK2;
+  }
+  if (cmdMode == MASK2) {
+    /* TODO: dispense second set of nozzles */
+    cmdMode = INSTRUCTION;
+    step();
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   while (Serial.available() <= 0) {
@@ -8,7 +46,24 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    int incomingByte = Serial.read();
+    unsigned char cmd = Serial.read();
     Serial.print('R'); // let host know byte has been received
+    if (cmdMode == INSTRUCTION) {
+      switch (cmd) {
+        case 'D':
+          cmdMode = MASK1;
+          break;
+        case 'R':
+          gotoBeginLine();
+          break;
+        case 'N':
+          gotoNextLine();
+          break;
+        default:
+          break;
+      }
+    } else {
+      dispense(cmd);
+    }
   }
 }
