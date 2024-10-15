379a5888bd23e71fbdd1f7c04781218daec6ec71
Mate Narh <spartaninzaghi@gmail.com>
2024-10-15 00:55:52 -0400

    pinInfo array created for all uno pins. ddr pinMode implemented

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -17,31 +17,70 @@ struct PinInfo {
   uint8_t bit;
 };
 
-// Nozzle Number       1  2  3  4  5  6  7  8  9   10  11  12 
-const int nozzles[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15};
-
-const PinInfo nozzlePins[] = {
-//                           Register  Pin Nozzle
-  {&DDRD, &PIND, &PORTD, 2}, // D2     2   1
-  {&DDRD, &PIND, &PORTD, 3}, // D3     3   2
-  {&DDRD, &PIND, &PORTD, 4}, // D4     4   3
-  {&DDRD, &PIND, &PORTD, 5}, // D5     5   4
-  {&DDRD, &PIND, &PORTD, 6}, // D6     6   5
-  {&DDRD, &PIND, &PORTD, 7}, // D7     7   6
-  {&DDRB, &PINB, &PORTB, 0}, // B0     8   7
-  {&DDRB, &PINB, &PORTB, 1}, // B1     9   8
-  {&DDRB, &PINB, &PORTB, 2}, // B2    10   9
-  {&DDRB, &PINB, &PORTB, 3}, // B3    11   10
-  {&DDRB, &PINB, &PORTB, 4}, // B4    12   11 
-  {&DDRC, &PINC, &PORTC, 1}, // C0    14   12
+const PinInfo allPins[] = {
+//                            GPIO    PIN
+  {&DDRD, &PIND, &PORTD, 0}, // D0     0   Rx     
+  {&DDRD, &PIND, &PORTD, 1}, // D1     1   Tx
+  {&DDRD, &PIND, &PORTD, 2}, // D2     2   
+  {&DDRD, &PIND, &PORTD, 3}, // D3     3   
+  {&DDRD, &PIND, &PORTD, 4}, // D4     4   
+  {&DDRD, &PIND, &PORTD, 5}, // D5     5   
+  {&DDRD, &PIND, &PORTD, 6}, // D6     6   
+  {&DDRD, &PIND, &PORTD, 7}, // D7     7   
+  {&DDRB, &PINB, &PORTB, 0}, // B0     8   
+  {&DDRB, &PINB, &PORTB, 1}, // B1     9   
+  {&DDRB, &PINB, &PORTB, 2}, // B2     10   
+  {&DDRB, &PINB, &PORTB, 3}, // B3     11   
+  {&DDRB, &PINB, &PORTB, 4}, // B4     12  
+  {&DDRB, &PINB, &PORTB, 5}, // B5     13  on-board LED    
+  {&DDRC, &PINC, &PORTC, 0}, // C0     14
+  {&DDRC, &PINC, &PORTC, 1}, // C1     15   
+  {&DDRC, &PINC, &PORTC, 2}, // C2     16   
+  {&DDRC, &PINC, &PORTC, 3}, // C3     17   
+  {&DDRC, &PINC, &PORTC, 4}, // C4     18   
+  {&DDRC, &PINC, &PORTC, 5}, // C5     19   
 };
 
+const PinInfo nozzlePins[] = {
+//                            GPIO    PIN   
+  {&DDRD, &PIND, &PORTD, 2}, // D2     2  
+  {&DDRD, &PIND, &PORTD, 3}, // D3     3  
+  {&DDRD, &PIND, &PORTD, 4}, // D4     4  
+  {&DDRD, &PIND, &PORTD, 5}, // D5     5  
+  {&DDRD, &PIND, &PORTD, 6}, // D6     6  
+  {&DDRD, &PIND, &PORTD, 7}, // D7     7  
+  {&DDRB, &PINB, &PORTB, 0}, // B0     8  
+  {&DDRB, &PINB, &PORTB, 1}, // B1     9  
+  {&DDRB, &PINB, &PORTB, 2}, // B2     10  
+  {&DDRB, &PINB, &PORTB, 3}, // B3     11  
+  {&DDRB, &PINB, &PORTB, 4}, // B4     12   
+  {&DDRC, &PINC, &PORTC, 0}, // C0     14  
+};
+
+
 /**
- * Set the pin for a nozzle HIGH or LOW
+ * Set up specified pin as INPUT or OUTPUT in data direction register
+ * @param info The register info for the nozzle pin to modify
+ * @param mode 1 to make specified pin OUTPUT, 0 for INPUT
+ */
+void pinModeDirect(const PinInfo& info, bool mode) {
+  volatile uint8_t ddr = *info.ddr;
+
+  if (mode) {
+    ddr |= 1 << info.bit;    
+  } else {
+    ddr &= ~(1 << info.bit);
+  }
+  
+  *info.ddr = ddr;
+}
+
+/**
+ * Write to a pin using direct port manipulation for low latency
  * @param info The register info for the nozzle pin to modify
  * @param state 1 (HIGH) to set pin to 5V, 0 (LOW) to set pin to 0V
  */
-void writePin(const PinInfo& info, bool state) {
+void digitalWriteFast(const PinInfo& info, bool state) {
   volatile uint8_t portRegister = *info.port;
 
   if (state) {
@@ -54,23 +93,25 @@ void writePin(const PinInfo& info, bool state) {
 }
 
 /**
- * Initiate a 5V pulse on a single nozzle
- *
- * For HP6602, A pulse lasts for 5us, after which follows a delay. 
- * For the same nozzle, an 800us is required for successive pulses. 
- * For different nozzles, a 0.5us delay is required. 
- * @param info The register info for nozzle pin to set or clear
+ * Pulse specified pin for PULSE_DURATION us
+ * @param info The pin info for nozzle pin to pulse
  */
-void pulse(const PinInfo& info){
-  writePin(info, HIGH); 
+void pulse(const PinInfo& info) {
+  digitalWriteFast(info, HIGH); 
   delayMicroseconds(PULSE_DURATION);
-  writePin(info, LOW);
+  digitalWriteFast(info, LOW);
 }
 
 /**
- * Test all nozzle pins successively
+ * Initiate a 5V pulse on a single nozzle
+ *
+ * For HP6602, A pulse lasts for 5us, after which follows a delay. 
+ * To successively pulse the same nozzle, an ~800us is required. 
+ * For different nozzles, a ~0.5us delay is required. 1us is used. See
+ * Inkshield theory: http://nicholasclewis.com/projects/inkshield/theory/ 
+ * @param info The register info for nozzle pin to set or clear
  */
-void pulseTestSuccessive(){
+void pulseTestSuccessive() {
   for (int i = 0; i < 12; i++){
     pulse(nozzlePins[i]);
     delayMicroseconds(PULSE_DELAY_DIFF_NOZZLE);
@@ -98,7 +139,6 @@ void gotoNextLine() {
 // when last mask done, call step() and change cmdMode to INSTRUCTION
 void dispense(unsigned char mask) {
   if (cmdMode == MASK1) {
-    /* TODO: dispense first set of nozzles */
     for (int i = 0; i < 8; i++){
       // pulse nozzle at index `i` if its bit is set HIGH in mask
       if (mask & (1 << i)){
@@ -108,11 +148,10 @@ void dispense(unsigned char mask) {
     cmdMode = MASK2;
   }
   if (cmdMode == MASK2) {
-    /* TODO: dispense second set of nozzles */
-    for (int i = 8; i < 12; i++){
-      // pulse nozzle at index `i` if its bit is set HIGH in mask
+    for (int i = 0; i < 4; i++){
       if (mask & (1 << i)){
-        pulse(nozzlePins[i]);
+        // pulse nozzle at index `i + 8` for remaining 4 nozzles
+        pulse(nozzlePins[i + 8]);
       }
     }
     cmdMode = INSTRUCTION;
@@ -129,7 +168,7 @@ void setup() {
 
   // Nozzle pin setup
   for(int i = 0; i < 12; i++){
-    pinMode(nozzles[i], OUTPUT);
+    pinModeDirect(nozzlePins[i], OUTPUT);
   }
 }
 
@@ -148,7 +187,7 @@ void loop() {
         case 'N':
           gotoNextLine();
           break;
-        case 'T': // Potential INSTRUCTION to test nozzles
+        case 'T': 
           pulseTestSuccessive();
           break;
         default:
