b776195f14fa498e6b8b456efdeb881f41385f03
Andrew Barton <33651875+abrtn@users.noreply.github.com>
2024-10-15 19:41:20 -0400

    Clean up code and add general single step function

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,15 @@
-#define dirPin_x 2
-#define stepPin_x 3
-#define dirPin_y 4
-#define stepPin_y 5
+#define DROPS_PER_INCH 96
+#define INCHES_PER_REV 1.5748 // 40 mm per revolution
+#define STEPS_PER_REV 3200    // 200 full steps * 16 microsteps
+#define STEPS_PER_DROP 21     // (1/96)*(1/1.5748)*3200 = 21.17
+#define NOZZLE_COUNT 1
+
+#define MOTOR_X_DIR_PIN 2
+#define MOTOR_X_STEP_PIN 3
+#define MOTOR_X_LIMIT_PIN 6
+#define MOTOR_Y_DIR_PIN 3
+#define MOTOR_Y_STEP_PIN 4
+#define MOTOR_Y_LIMIT_PIN 7
 
 enum CommandMode {
   INSTRUCTION,
@@ -9,41 +17,46 @@ enum CommandMode {
   MASK2,
 };
 
-CommandMode cmdMode = INSTRUCTION;
+struct StepperMotor {
+  int dirPin;
+  int stepPin;
+  int limitPin;
+};
 
-// move stepper x 1 step (dot width) to the right
+CommandMode cmdMode = INSTRUCTION;
+StepperMotor motorX = {MOTOR_X_DIR_PIN, MOTOR_X_STEP_PIN, MOTOR_X_LIMIT_PIN};
+StepperMotor motorY = {MOTOR_Y_DIR_PIN, MOTOR_Y_STEP_PIN, MOTOR_Y_LIMIT_PIN};
+
+int stepsTaken = 0;
+
+// move stepper 1 step (dot width) to the right
 // keep track of steps moved so we can return to the begin of line
-void step() {
-  // 21 microsteps per drop
-  digitalWrite(dirPin_x, LOW);
-  for(int i = 0; i < 21; i++){
-    digitalWrite(stepPin_x, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin_x, LOW);
-    delayMicroseconds(1000);
+void step(const StepperMotor& motor = motorX, int dir = 0) {
+  digitalWrite(motor.dirPin, dir);
+  digitalWrite(motor.stepPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(motor.stepPin, LOW);
+  delayMicroseconds(1000);
+}
+
+void stepPerDrop(const StepperMotor& motor = motorX, int dir = 0) {
+  for(int i = 0; i < STEPS_PER_DROP; i++){
+    step(motor, dir);
   }
 }
 
 // goto the begin of current line
-void gotoBeginLine(int steps) {
-  digitalWrite(dirPin_x, HIGH);
-  for(int i = 0; i < 21*steps; i++){
-    digitalWrite(stepPin_x, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin_x, LOW);
-    delayMicroseconds(1000);
+void gotoBeginLine() {
+  for(int i = 0; i < stepsTaken; i++){
+    stepPerDrop(motorX, 1);  // does not allow for speed control
   }
+  stepsTaken = 0;
 }
 
 // move stepper y 1 line above (dot height) * nozzleCount
 void gotoNextLine() {
-  int nozzleCount = 1;
-  digitalWrite(dirPin_y, LOW);
-  for(int i = 0; i < 21*nozzleCount; i++){
-    digitalWrite(stepPin_y, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin_y, LOW);
-    delayMicroseconds(1000);
+  for(int i = 0; i < NOZZLE_COUNT; i++){
+    stepPerDrop(motorY);
   }
 }
 
@@ -57,52 +70,44 @@ void dispense(unsigned char mask) {
   if (cmdMode == MASK2) {
     /* TODO: dispense second set of nozzles */
     cmdMode = INSTRUCTION;
-    step();
+    stepPerDrop();
+    stepsTaken += 1;
   }
 }
 
 void setup() {
-  pinMode(stepPin_x, OUTPUT);
-  pinMode(dirPin_x, OUTPUT);
-  pinMode(stepPin_y, OUTPUT);
-  pinMode(dirPin_y, OUTPUT);
+  pinMode(motorX.dirPin, OUTPUT);
+  pinMode(motorX.stepPin, OUTPUT);
+  pinMode(motorY.dirPin, OUTPUT);
+  pinMode(motorY.stepPin, OUTPUT);
   
-  // Serial.begin(115200);
-  // while (Serial.available() <= 0) {
-  //   Serial.print('X');
-  //   delay(100);
-  // }
+  Serial.begin(115200);
+  while (Serial.available() <= 0) {
+    Serial.print('X');
+    delay(100);
+  }
 }
 
 void loop() {
-
-  for(int i = 0; i < 50; i++) {
-    step();
+  if (Serial.available() > 0) {
+    unsigned char cmd = Serial.read();
+    Serial.print('R'); // let host know byte has been received
+    if (cmdMode == INSTRUCTION) {
+      switch (cmd) {
+        case 'D':
+          cmdMode = MASK1;
+          break;
+        case 'R':
+          gotoBeginLine();
+          break;
+        case 'N':
+          gotoNextLine();
+          break;
+        default:
+          break;
+      }
+    } else {
+      dispense(cmd);
+    }
   }
-  gotoBeginLine(50);
-  for(int i = 0; i < 10; i++) {
-    gotoNextLine();
-  }
-  
-  // if (Serial.available() > 0) {
-  //   unsigned char cmd = Serial.read();
-  //   Serial.print('R'); // let host know byte has been received
-  //   if (cmdMode == INSTRUCTION) {
-  //     switch (cmd) {
-  //       case 'D':
-  //         cmdMode = MASK1;
-  //         break;
-  //       case 'R':
-  //         gotoBeginLine();
-  //         break;
-  //       case 'N':
-  //         gotoNextLine();
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   } else {
-  //     dispense(cmd);
-  //   }
-  // }
 }
